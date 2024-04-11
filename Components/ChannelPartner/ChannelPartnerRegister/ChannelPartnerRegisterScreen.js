c895ecae77ab1c6569eb9cb75dddbf3750737cb9
akshitkumar <akshitkumar383@gmail.com>
2024-04-11 22:15:02 +0530

    akl

--- Components/ChannelPartner/ChannelPartnerRegister/ChannelPartnerRegisterScreen.js
+++ Components/ChannelPartner/ChannelPartnerRegister/ChannelPartnerRegisterScreen.js
@@ -1,12 +1,23 @@
 import React, { useState } from 'react';
 import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
 import SaveIcon from '@mui/icons-material/Save';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { toast } from 'react-toastify';
+import { Baseurl } from '../../../Utils/Constants';
+import axios from 'axios';
 
 const ChannelPartnerRegisterScreen = () => {
   const [aadharCardPreview, setAadharCardPreview] = useState(null);
   const [panCardPreviews, setPanCardPreviews] = useState([]);
   const [reraLicensePreviews, setReraLicensePreviews] = useState([]);
   const [bankChequePreviews, setBankChequePreviews] = useState([]);
+  const [name,setName]=useState("")
+  const [email,setEmail]=useState("")
+  const [mobile,setMobile]=useState("")
+  const[isTokenVerified,setIsTokenVerified]=useState(false)
+  const router=useRouter();
+  const {token}=router.query
 
   const handleAadharCardChange = (event) => {
     const file = event.target.files[0];
@@ -70,6 +81,31 @@ const ChannelPartnerRegisterScreen = () => {
     }
   };
 
+  const verifyToken=async()=>{
+          try {
+            const { data } = await axios.post(Baseurl + `/db/users/cp/registrationToken/verification`,{token});
+            if(data.status===200){
+              toast.success(data.message);
+              setName(data.data.user)
+              setMobile(data.data.contact_number)
+              setEmail(data.data.email)
+              setIsTokenVerified(true)
+            }
+           
+          } catch (error) {
+            
+            if (error?.response?.data?.message) {
+              toast.success(error.response.data.message);
+            } else {
+              toast.error("Something went wrong!");
+            }
+          }
+  }
+
+  useEffect(()=>{
+    verifyToken()
+  })
+
   return (
     <div className='d-block w-100'>
       <section className="channel_partner_register">
@@ -97,15 +133,15 @@ const ChannelPartnerRegisterScreen = () => {
                 <div className="row">
                   <div className="col-xl-3 col-md-3 col-lg-3 col-sm-12  mb-3">
                     <label className="form-label">Name <span className="error-message">*</span></label>
-                    <input className="form-control input-field" type="text" placeholder="Enter Name" id="Name" formcontrolname="Email" name="Name" />
+                    <input className="form-control input-field" type="text" placeholder="Enter Name" id="Name" formcontrolname="Email" name="Name" value={name} disabled={isTokenVerified} />
                   </div>
                   <div className="col-xl-3 col-md-3 col-lg-3 col-sm-12  mb-3">
                     <label className="form-label">Email <span className="error-message">*</span></label>
-                    <input className="form-control input-field" type="text" placeholder="Enter Email" id="Email" formcontrolname="Email" name="Email" />
+                    <input className="form-control input-field" type="text" placeholder="Enter Email" id="Email" formcontrolname="Email" name="Email" value={email} disabled={isTokenVerified} />
                   </div>
                   <div className="col-xl-3 col-md-3 col-lg-3 col-sm-12  mb-3">
                     <label className="form-label">Mobile No. <span className="error-message">*</span></label>
-                    <input className="form-control input-field" formcontrolname="Name" type="text" placeholder="Enter Mobile No." name="Mobile" />
+                    <input className="form-control input-field" formcontrolname="Name" type="text" placeholder="Enter Mobile No." name="Mobile" value={mobile} disabled={isTokenVerified} />
                   </div>
                   <div className="col-12 mb-2">
                   </div>
@@ -119,7 +155,7 @@ const ChannelPartnerRegisterScreen = () => {
                   <div className="col-xl-3 col-md-3 col-lg-3 col-sm-12  mb-3">
                   <div className="d-flex flex-column gap-1">
                   <label className="form-label">PAN Card <span className="error-message">*</span></label>
-                    <input type="file" onChange={handlePanCardChange} multiple className="form-control input-field" />
+                    <input type="file" onChange={handlePanCardChange}  className="form-control input-field" />
                     {panCardPreviews.map((preview, index) => (
                       <img key={index} src={preview} alt={`PAN Card Preview ${index}`} style={{ maxWidth: '100px', maxHeight: '100px', marginRight: '5px' }} />
                     ))}
@@ -128,7 +164,7 @@ const ChannelPartnerRegisterScreen = () => {
                   <div className="col-xl-3 col-md-3 col-lg-3 col-sm-12  mb-3">
                   <div className="d-flex flex-column gap-1">
                   <label className="form-label">RERA License <span className="error-message">*</span></label>
-                    <input type="file" onChange={handleReraLicenseChange} multiple className="form-control input-field" />
+                    <input type="file" onChange={handleReraLicenseChange}  className="form-control input-field" />
                     {reraLicensePreviews.map((preview, index) => (
                       <img key={index} src={preview} alt={`RERA License Preview ${index}`} style={{ maxWidth: '100px', maxHeight: '100px', marginRight: '5px' }} />
                     ))}
@@ -137,7 +173,7 @@ const ChannelPartnerRegisterScreen = () => {
                   <div className="col-xl-3 col-md-3 col-lg-3 col-sm-12  mb-3">
                   <div className="d-flex flex-column gap-1">
                   <label className="form-label">Bank Cancelled Cheque <span className="error-message" /></label>
-                    <input type="file" onChange={handleBankChequeChange} multiple className="form-control input-field" />
+                    <input type="file" onChange={handleBankChequeChange}  className="form-control input-field" />
                     {bankChequePreviews.map((preview, index) => (
                       <img key={index} src={preview} alt={`Bank Cancelled Cheque Preview ${index}`} style={{ maxWidth: '100px', maxHeight: '100px', marginRight: '5px' }} />
                     ))}
@@ -160,3 +196,4 @@ const ChannelPartnerRegisterScreen = () => {
 };
 
 export default ChannelPartnerRegisterScreen;
+

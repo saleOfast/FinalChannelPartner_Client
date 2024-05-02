da5c27c1987cd69907011e12e37124fb187f4d40
akshitkumar <akshitkumar383@gmail.com>
2024-05-02 20:18:19 +0530

    ada

--- Components/ChannelPartner/User/Bookings/ManageUsersTable.js
+++ Components/ChannelPartner/User/Bookings/ManageUsersTable.js
@@ -96,7 +96,7 @@ const ManageUsersTable = ({ deleteConfirm, disableConfirm, dataList, openEdtMdl,
         customBodyRender: (value, tableMeta, updateValue) => {
           return (
             <Link href={`/CHANNEL/BookingDetails?booking_id=${tableMeta?.rowData[0]}`} className='status_box fw-bold text-decoration-underline' style={{ color: "#293790" }}>
-              {value?.lead_name}
+              {value}
             </Link>
           )
         }

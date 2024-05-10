3d47a1dd08177c49fe6e41e03a63329d399aca11
akshitkumar <akshitkumar383@gmail.com>
2024-05-10 17:02:26 +0530

    hfh

--- Components/ChannelPartner/Admin/DashboardAdmin/ReChart.js
+++ Components/ChannelPartner/Admin/DashboardAdmin/ReChart.js
@@ -7,7 +7,11 @@ const ReChart = ({ head, dataList}) => {
         <>
                  <div className="row pb-0">
                     <div className="col-xl-8 col-md-8 col-sm-6 col-6">
+                    <div className='fw-bold fs-5 '>
+                        {head}
+                        </div>
                         <div className="pieChart pt-5">
+                            
                             <Charts 
                              dataList={dataList}
                             />
@@ -15,7 +19,7 @@ const ReChart = ({ head, dataList}) => {
                     </div>
                     <div className="col-xl-4 col-md-4 col-sm-6 col-6" style={{maxWidth:"150px"}}>
                         <div className="detail_sec">
-                            <div className="head_text" style={{fontSize:"20px"}}>{head}</div>
+                            <div className="head_text opacity-0 " style={{fontSize:"20px"}}>{head}</div>
                         </div>
                     </div>
             </div>
--- Components/ChannelPartner/User/DashboardUser/ReChart.js
+++ Components/ChannelPartner/User/DashboardUser/ReChart.js
@@ -7,6 +7,9 @@ const ReChart = ({ head, dataList}) => {
         <>
                 <div className="row pb-0">
                     <div className="col-xl-8 col-md-8 col-sm-6 col-6">
+                        <div className='fw-bold fs-5'>
+                        {head}
+                        </div>
                         <div className="pieChart pt-5">
                             <Charts 
                              dataList={dataList}
@@ -15,7 +18,7 @@ const ReChart = ({ head, dataList}) => {
                     </div>
                     <div className="col-xl-4 col-md-4 col-sm-6 col-6">
                         <div className="detail_sec">
-                            <div className="head_text" style={{fontSize:"20px"}}>{head}</div>
+                            <div className="head_text opacity-0" style={{fontSize:"20px"}}>{head}</div>
                         </div>
                     </div>
             </div>

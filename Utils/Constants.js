56346d145280f015a48155ebf48e4c379bd779de
vishal jais <vishal.jais00@gmail.com>
2024-05-30 15:07:41 +0530

    hg

--- Utils/Constants.js
+++ Utils/Constants.js
@@ -1,8 +1,8 @@
 
 /*  -----------------uat links------------- */
 
-export const Baseurl = 'http://crm.saleofast.com:8050/api/v1';
-export const filesUrl = 'http://crm.saleofast.com:8050/images'; 
+// export const Baseurl = 'http://crm.saleofast.com:8050/api/v1';
+// export const filesUrl = 'http://crm.saleofast.com:8050/images'; 
 
 /*  -----------------iis links------------- */
 
@@ -12,5 +12,5 @@ export const filesUrl = 'http://crm.saleofast.com:8050/images';
 
 /* -----------------main links------------- */
 
-// export const Baseurl = 'http://localhost:8050/api/v1';
-// export const filesUrl = 'http://localhost:8050/images'; 
+export const Baseurl = 'http://localhost:8050/api/v1';
+export const filesUrl = 'http://localhost:8050/images'; 

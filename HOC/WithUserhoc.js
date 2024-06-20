984392ad63336a00c90399d5d79d119e030e92b0
akshitkumar <akshitkumar383@gmail.com>
2024-06-20 19:47:52 +0530

    illegal route

--- HOC/WithUserhoc.js
+++ HOC/WithUserhoc.js
@@ -1,3 +1,51 @@
+// import { useEffect, useState } from 'react';
+// import { useRouter } from 'next/router';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { hasCookie } from 'cookies-next';
+// import { clearTheme } from '../store/themeSlice';
+// import { clearValue } from '../store/permissionSlice';
+// import { LoggedOut } from '../store/adMinLoginSlice';
+// import { userLogOut } from '../store/ClientLoginSlice';
+// import { toast } from 'react-toastify';
+
+// const withUser = (WrappedComponent) => {
+//   return ({ ...rest }) => {
+//     const router = useRouter();
+//     const dispatch=useDispatch()
+//     const userLogin = useSelector((state) => state.userLogin.value)
+//     const [rendercomponent, setRendercomponent] = useState(false)
+//     const dbMode = useSelector((state) => state.dbMode.value);
+     
+
+//     useEffect(() => {
+      
+//       if (hasCookie("SaLsUsr")) {
+//         router.push('/Admin');
+//       }else if(!hasCookie("user")){
+//         router.push('/');
+        
+//       }
+//       else if(!hasCookie("crm")){
+//        dispatch(clearTheme());
+//     dispatch(clearValue())
+//     const isAdminMode = dbMode === "admin";
+//     dispatch(isAdminMode ? LoggedOut()  : userLogOut()); 
+//     toast.warning("Illegal Route Access")
+//          router.push('/')
+//       }
+//       else{
+//         setRendercomponent(true)
+//       }
+      
+//     }, [userLogin]);
+
+//     return rendercomponent ? <WrappedComponent {...rest} /> : null ;
+//   };
+// };
+
+// export default withUser;
+
+
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { useSelector } from 'react-redux';
--- HOC/WithUserhoc_CP.js
+++ HOC/WithUserhoc_CP.js
@@ -1,3 +1,49 @@
+// import { useEffect, useState } from 'react';
+// import { useRouter } from 'next/router';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { hasCookie } from 'cookies-next';
+// import { clearTheme } from '../store/themeSlice';
+// import { clearValue } from '../store/permissionSlice';
+// import { LoggedOut } from '../store/adMinLoginSlice';
+// import { userLogOut } from '../store/ClientLoginSlice';
+// import { toast } from 'react-toastify';
+
+// const WithUserhoc_CP = (WrappedComponent) => {
+//   return ({ ...rest }) => {
+//     const router = useRouter();
+//     const dispatch=useDispatch()
+//     const userLogin = useSelector((state) => state.userLogin.value)
+//     const [rendercomponent, setRendercomponent] = useState(false)
+//     const dbMode = useSelector((state) => state.dbMode.value);
+
+//     useEffect(() => {
+      
+//       if (hasCookie("SaLsUsr")) {
+//         router.push('/Admin');
+//       }else if(!hasCookie("user")){
+//         router.push('/partner');
+        
+//       }
+//       else if(!hasCookie("channel")){
+//                dispatch(clearTheme());
+//             dispatch(clearValue())
+//             const isAdminMode = dbMode === "admin";
+//             dispatch(isAdminMode ? LoggedOut()  : userLogOut()); 
+//             toast.warning("Illegal Route Access")
+//                  router.push('/')
+//               }
+//       else{
+//         setRendercomponent(true)
+//       }
+      
+//     }, [userLogin]);
+
+//     return rendercomponent ? <WrappedComponent {...rest} /> : null ;
+//   };
+// };
+
+// export default WithUserhoc_CP;
+
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { useSelector } from 'react-redux';
@@ -28,3 +74,4 @@ const WithUserhoc_CP = (WrappedComponent) => {
 };
 
 export default WithUserhoc_CP;
+
